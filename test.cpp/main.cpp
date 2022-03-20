9fab86f76b686c46b8afb4440ccbf77770525807
PocketBrain <pocketbaincorp@gmail.com>
2022-03-20 21:36:04 +0300

    Initial Commit

---
+++ test.cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test.cpp
+//
+//  Created by Владислав on 20.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
